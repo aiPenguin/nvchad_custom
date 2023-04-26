f88e79f535d52c842e0ddf7f5c695a393d1e8fcf
aiPenguin <dzfwang@outlook.com>
2023-04-27 01:03:35 +0200

    use <A-#> to select buffer

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 local autocmd = vim.api.nvim_create_autocmd
 local opt = vim.opt
+local diagnostic = vim.diagnostic
 
 -- Auto resize panes when resizing nvim window
 autocmd("VimResized", {
@@ -9,3 +10,12 @@ autocmd("VimResized", {
 
 -- colorcolumn
 -- opt.colorcolumn = "120"
+
+-- This utilizes the vim.t.bufs tab table variable where we store buffer numbers of current tab.
+-- Then it loops from 1-9 to create mappings of switching tab by number i.e Alt+1 will switch first tab.
+for i = 1, 9, 1 do
+  vim.keymap.set("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
+
