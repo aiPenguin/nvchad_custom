14ff95786fa048bc5f3dd87c585b4b8dc094c934
aiPenguin <dzfwang@outlook.com>
2023-04-27 14:56:47 +0200

    seperat plugin mappings

--- mappings.lua
+++ mappings.lua
@@ -5,13 +5,6 @@ M.general = {
   n = {
     -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>s"] = { "<cmd> w <CR>", "save file" },
-    ["<leader>m"] = { "<cmd> Telescope todo-comments <CR>", "show TODOs" },
-    ["<leader>ng"] = {
-      function()
-        require("neogen").generate()
-      end,
-      "generate annotation",
-    },
   },
 }
 
@@ -44,22 +37,67 @@ M.telescope = {
 
 M.gitsigns = {
   n = {
-    ["<leader>hs"] = { function() require("gitsigns").stage_hunk() end, "stage hunk" },
-    ["<leader>hr"] = { function() require("gitsigns").reset_hunk() end, "reset hunk" },
-    ["<leader>hS"] = { function() require("gitsigns").stage_buffer() end, "stage buffer" },
-    ["<leader>hu"] = { function() require("gitsigns").undo_stage_hunk() end, "undo stage hunk" },
-    ["<leader>hR"] = { function() require("gitsigns").reset_buffer() end, "reset buffer" },
-    ["<leader>hp"] = { function() require("gitsigns").preview_hunk() end, "preview hunk" },
+    ["<leader>hs"] = {
+      function()
+        require("gitsigns").stage_hunk()
+      end,
+      "stage hunk",
+    },
+    ["<leader>hr"] = {
+      function()
+        require("gitsigns").reset_hunk()
+      end,
+      "reset hunk",
+    },
+    ["<leader>hS"] = {
+      function()
+        require("gitsigns").stage_buffer()
+      end,
+      "stage buffer",
+    },
+    ["<leader>hu"] = {
+      function()
+        require("gitsigns").undo_stage_hunk()
+      end,
+      "undo stage hunk",
+    },
+    ["<leader>hR"] = {
+      function()
+        require("gitsigns").reset_buffer()
+      end,
+      "reset buffer",
+    },
+    ["<leader>hp"] = {
+      function()
+        require("gitsigns").preview_hunk()
+      end,
+      "preview hunk",
+    },
     ["<leader>hb"] = {
       function()
         require("gitsigns").blame_line { full = true }
       end,
       "blame line",
     },
-    ["<leader>hd"] = { function() require("gitsigns").diffthis() end, "diffthis" },
+    ["<leader>hd"] = {
+      function()
+        require("gitsigns").diffthis()
+      end,
+      "diffthis",
+    },
     ["<leader>hD"] = { "<cmd> diffoff <CR>", "closs diffview" },
-    ["<leader>td"] = {function() require("gitsigns").toggle_deleted() end, "toggle deleted" },
-    ["<leader>tb"] = {function() require("gitsigns").toggle_current_line_blame() end, "toggle current line blame" },
+    ["<leader>td"] = {
+      function()
+        require("gitsigns").toggle_deleted()
+      end,
+      "toggle deleted",
+    },
+    ["<leader>tb"] = {
+      function()
+        require("gitsigns").toggle_current_line_blame()
+      end,
+      "toggle current line blame",
+    },
   },
   v = {
     ["<leader>hs"] = {
@@ -84,6 +122,36 @@ M.gitsigns = {
     ["ih"] = { ":<C-U>Gitsigns select_hunk<CR>", "select hunk" },
   },
 }
+
+-- new plugin mappings
+M.todo_comments = {
+  plugin = true,
+  n = {
+    ["<leader>m"] = { "<cmd> Telescope todo-comments <CR>", "show TODOs" },
+  },
+}
+require("core.utils").load_mappings("todo_comments")
+
+M.noegen = {
+  plugin = true,
+  n = {
+    ["<leader>ng"] = {
+      function()
+        require("neogen").generate()
+      end,
+      "generate annotation",
+    },
+  },
+}
+require("core.utils").load_mappings("noegen")
+
+M.symbols_outline = {
+  plugin = true,
+  n = {
+    ['<leader>o'] = {"<cmd> SymbolsOutline <CR>", "toggle symbols outline"}
+  }
+}
+require("core.utils").load_mappings("symbols_outline")
 
 M.disabled = {
   n = {
